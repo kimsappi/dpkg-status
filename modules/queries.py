ce207ace2257e658807fdeddf1a4873e6f94bc78
Kim Sappi <ksappi@student.hive.fi>
2020-08-20 11:00:19 +0300

    Good progress on SQL variant

--- modules/queries.py
+++ modules/queries.py
@@ -17,18 +17,27 @@ def dictify(results: tuple, *columns):
 	"""
 	Converts the returned values to dictionary for easier handling.
 	"""
-	#print('asd################')
-	#print(columns)
-	#print('asd################')
 	if not len(results):
 		return None
 	elif type(results[0]) is not tuple:
-		return dictifySingle(results[0], columnDescriptionToArray(columns))
+		return dictifySingle(results, columnDescriptionToArray(columns))
 	else:
 		ret = []
 		for result in results:
 			ret.append(dictifySingle(result, columnDescriptionToArray(columns)))
 		return ret
+
+def separatePackageIdAndName(dependencies: str):
+	if not dependencies:
+		return None
+	dependencies = dependencies.split(' ')
+	ret = []
+	for i in range(0, len(dependencies), 2):
+		depDict = {}
+		depDict['id'] = dependencies[i]
+		depDict['name'] = dependencies[i + 1]
+		ret.append(depDict)
+	return ret
 
 def index():
 	dbConnection = DBConnection()
@@ -45,18 +54,35 @@ def package(id: str):
 
 	try: # Checking if user provided numeric id or package name
 		int(id)
-		number = True
+		condition = 'p.id = ?'
 	except:
-		number = False
+		condition = 'p."name" = ?'
 
-	if number:
-		query = """
-SELECT * FROM "packages" AS "p"
-	OUTER LEFT JOIN "dependencies" AS "d" ON "d"."dependent" = "p"."name"
-	OUTER LEFT JOIN "dependencies AS "r" ON "r"."dependency" = "r"."name";
+	depsQuery = f"""
+SELECT "name", "version", "description", descriptionSummary,
+		GROUP_CONCAT((SELECT id FROM packages WHERE name=d.dependency) || ' ' || d.dependency, ' ') AS dependencies
+	FROM packages AS p
+	OUTER LEFT JOIN dependencies AS d ON d.dependent = p."name"
+	WHERE {condition};
 """
-	cursor.execute(query)
+
+	revDepsQuery = f"""
+SELECT GROUP_CONCAT((SELECT id FROM packages WHERE name=r.dependent) || ' ' || r.dependent, ' ') AS reverseDependencies
+	FROM packages AS p
+	OUTER LEFT JOIN dependencies AS r ON r.dependency = p."name"
+	WHERE {condition};
+"""
+	cursor.execute(depsQuery, (id,))
 	result = cursor.fetchone()
 	cols = cursor.description
+	cursor.close()
+
+	cursor = dbConnection.connection.cursor()
+	cursor.execute(revDepsQuery, (id,))
+	reverseResult = cursor.fetchone()
+	reverseCols = cursor.description
 	dbConnection.close()
-	return dictify(result, cols)
+	result = dictify(result + reverseResult, cols + reverseCols)
+	result['dependencies'] = separatePackageIdAndName(result['dependencies'])
+	result['reverseDependencies'] = separatePackageIdAndName(result['reverseDependencies'])
+	return result
